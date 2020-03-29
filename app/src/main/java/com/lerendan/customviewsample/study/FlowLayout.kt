376c65e7d84105abf129e2410ab47bdf0d270b1b
lerendan <lerendan@gmail.com>
2020-03-29 18:39:37 +0800

    TagLayout rename

--- app/src/main/java/com/lerendan/customviewsample/study/FlowLayout.kt
+++ app/src/main/java/com/lerendan/customviewsample/study/FlowLayout.kt
@@ -8,37 +8,39 @@ import android.view.ViewGroup
 /**
  * Created by danchao on 2020/3/27.
  */
-class TagLayout(context: Context?, attrs: AttributeSet?) : ViewGroup(context, attrs) {
+class FlowLayout(context: Context?, attrs: AttributeSet?) : ViewGroup(context, attrs) {
 
+    //子view的rect
     var childrenBounds = ArrayList<Rect>()
 
-    init {
-
-    }
-
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val widthSpecMode = MeasureSpec.getMode(widthMeasureSpec)
+        val widthSpecSize = MeasureSpec.getSize(widthMeasureSpec)
 
+        //已使用宽度
         var widthUsed = 0
+        //已使用高度
         var heightUsed = 0
+        //当前行width已使用的宽度
         var lineWidthUsed = 0
+        //当前行view的高度最大值
         var lineMaxHeight = 0
 
-        var specMode = MeasureSpec.getMode(widthMeasureSpec)
-        var specWidth = MeasureSpec.getSize(widthMeasureSpec)
-
         for (i in 0 until childCount) {
+            //获取子view
             val childView = getChildAt(i)
+            //测量子view，因为要换行，所以这里的widthUsed不传入，我们下面自己计算
+            measureChildWithMargins(childView, widthMeasureSpec, 0, heightMeasureSpec, 0)
 
-            measureChildWithMargins(childView, widthMeasureSpec, 0, heightMeasureSpec, heightUsed)
-
-            if (specMode != MeasureSpec.UNSPECIFIED &&
-                lineWidthUsed + childView.measuredWidth > specWidth) {
+            //换行逻辑
+            if (widthSpecMode != MeasureSpec.UNSPECIFIED && lineWidthUsed + childView.measuredWidth > widthSpecSize) {
                 lineWidthUsed = 0
                 heightUsed += lineMaxHeight
                 lineMaxHeight = 0
                 measureChildWithMargins(childView, widthMeasureSpec, 0, heightMeasureSpec, heightUsed)
             }
 
+            //避免重复创建
             var childBound: Rect
             if (childrenBounds.size <= i) {
                 childBound = Rect()
@@ -47,18 +49,23 @@ class TagLayout(context: Context?, attrs: AttributeSet?) : ViewGroup(context, at
                 childBound = childrenBounds[i]
             }
 
+            //设置rect边界
             childBound.set(
-                lineWidthUsed, heightUsed,
-                lineWidthUsed + childView.measuredWidth, heightUsed + childView.measuredHeight
+                lineWidthUsed, heightUsed, lineWidthUsed + childView.measuredWidth,
+                heightUsed + childView.measuredHeight
             )
 
+            //当前行使用宽度加上当前childView的测量宽度
             lineWidthUsed += childView.measuredWidth
+            //计算最大的宽度
             widthUsed = Math.max(widthUsed, lineWidthUsed)
+            //当前行childView的最大高度
             lineMaxHeight = Math.max(lineMaxHeight, childView.measuredHeight)
 
         }
-        var width = widthUsed
-        var height = heightUsed + lineMaxHeight
+        val width = widthUsed
+        //viewGroup的使用高度要加上最后一行的最大高度
+        val height = heightUsed + lineMaxHeight
         setMeasuredDimension(width, height)
     }
 
